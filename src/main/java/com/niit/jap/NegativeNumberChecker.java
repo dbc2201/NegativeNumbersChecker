460cb5292d0a10c5e1068fc3d1c08597921bf065
dbc2201 <divyanshb22@gmail.com>
2022-06-16 22:36:19 +0530

    add the method to be completed

--- src/main/java/com/niit/jap/NegativeNumberChecker.java
+++ src/main/java/com/niit/jap/NegativeNumberChecker.java
@@ -1,5 +1,14 @@
 package com.niit.jap;
 
 public class NegativeNumberChecker {
-
+    /**
+     * This method checks whether a number is negative or not.
+     *
+     * @param number the number to check.
+     * @return true if the number is negative, otherwise false.
+     */
+    public boolean isNegativeNumber(int number) {
+        // TODO complete this method!
+        return false;
+    }
 }
