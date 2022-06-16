2aaf1b2bc6c5123b96eb17ad326934ad8838833d
dbc2201 <divyanshb22@gmail.com>
2022-06-16 22:43:27 +0530

    complete the boilerplate code

--- src/main/java/com/niit/jap/Main.java
+++ src/main/java/com/niit/jap/Main.java
@@ -1,7 +1,15 @@
 package com.niit.jap;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NegativeNumberChecker negativeNumberChecker = new NegativeNumberChecker();
+        System.out.println("Please enter a number to check: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        boolean isNegativeNumber = negativeNumberChecker.isNegativeNumber(number);
+        System.out.println("Is " + number + " a negative number? " + isNegativeNumber);
+        scanner.close();
     }
 }
---
+++ src/test/java/com/niit/jap/NegativeNumberCheckerTest.java
@@ -0,0 +1,22 @@
+package com.niit.jap;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NegativeNumberCheckerTest {
+
+    private NegativeNumberChecker negativeNumberChecker;
+
+    @BeforeEach
+    void setUp() {
+        negativeNumberChecker = new NegativeNumberChecker();
+    }
+
+    @ParameterizedTest(name = "isNegativeNumber({arguments})")
+    @ValueSource(ints = {Integer.MIN_VALUE, -100, -50, -10})
+    void isNegativeNumber(int number) {
+        Assertions.assertTrue(negativeNumberChecker.isNegativeNumber(number));
+    }
+}
